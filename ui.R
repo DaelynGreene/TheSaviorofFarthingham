c575c4c72d16bb6258af82915b30b485c1a3560c
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-21 14:02:25 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,10 +3,10 @@ library(shinyWidgets)
 
 ui<-fluidPage(
   setBackgroundImage(BackgroundImage),
-  h2(BackstoryText1,align="center")
+  h2(BackstoryText1,align="center"),
   
   
-  
+  actionButton("run","Run Analysis", class = "btn-link")
   
   
 )
