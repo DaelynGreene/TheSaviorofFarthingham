8d0f6cbd2087450f73edb42350a9644a537cf11a
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-28 15:32:46 -0400

    adam's wizardry made it work

--- global.R
+++ global.R
@@ -1,6 +1,8 @@
+library(shiny)
+library(shinyWidgets)
+
 BackstoryText1<-"You are journeying from a neighboring kingdom at the urging of your sister's last letter."
 BackstoryText2<-"Before she died, she wrote you, pleading for you to come to Farthingham and overthrow the evil ruler Henry Shelburton."
 BackstoryText3<-"This is your story."
 
 TitleWords<-"The Savior of Farthingham"
-BackgroundImage<-c(src="https://drive.google.com/file/d/15YEYfDGcLxthSBF-f6yQnxWs_efbdWj7/view",shinydashboard=F)
--- ui.R
+++ ui.R
@@ -1,12 +1,23 @@
-library(shiny)
-library(shinyWidgets)
-
 ui<-fluidPage(
-  setBackgroundImage(BackgroundImage),
-  h2(BackstoryText1,align="center"),
+  # h2(BackstoryText1,align="center"),
   
+  tags$style("@import url(https://use.fontawesome.com/releases/v6.1.0/css/all.css);"),
+  tags$head( tags$style(HTML(".fa{font-size: 50px;}"))),
   
-  actionButton("run","Run Analysis", class = "btn-link")
+  #change the color of links with a tag$href or a or something
   
+  actionButton("run","beer-mug-empty", class = "btn-link btn-lg", icon = icon("beer-mug-empty")),
+  actionButton("run","bandage", class = "btn-link btn-lg",icon = icon("bandage")),
+  actionButton("run","archway", class = "btn-link btn-lg",icon = icon("archway")),
+  actionButton("run","apple-whole", class = "btn-link btn-lg",icon = icon("apple-whole")),
+  actionButton("run","bottle-droplet", class = "btn-link btn-lg",icon = icon("bottle-droplet")),
+  actionButton("run","sack-dollar", class = "btn-link btn-lg",icon = icon("sack-dollar")),
+  actionButton("run","cash-register", class = "btn-link btn-lg",icon = icon("cash-register")),
+  actionButton("run","dungeon", class = "btn-link btn-lg",icon = icon("dungeon")),
+  actionButton("run","fort-awesome", class = "btn-link btn-lg",icon = icon("fort-awesome")),
+  actionButton("run","horse", class = "btn-link btn-lg",icon = icon("horse")),
+  actionButton("run","mortar-pestle", class = "btn-link btn-lg",icon = icon("mortar-pestle")),
+  actionButton("run","scale-balanced ", class = "btn-link btn-lg",icon = icon("scale-balanced")),
+  icon("beer-mug-empty")
   
 )
