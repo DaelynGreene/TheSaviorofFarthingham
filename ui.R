02625f59f4e03e0093bbf367d88a59fed2675a55
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-30 13:42:40 -0400

    the town menu is alive

--- ui.R
+++ ui.R
@@ -2,28 +2,101 @@ ui<-fluidPage(
   # h2(BackstoryText1,align="center"),
   
   tags$style("@import url(https://use.fontawesome.com/releases/v6.1.1/css/all.css);",
-             HTML(".fa{font-size: 100px; color:#FF8200; align-middle}"),
+             HTML(".fa{font-size: 80px; color:#FF8200; align-middle}"),
              HTML(".fa-fort-awesome{font-size: 100px; color:#FF8200}"),
-             HTML("#run1{color: red}")
-             
-  ),
-  
-  #change the color of links with a tag$href or a or something
+             HTML("#run1{color: green}"),
+             HTML("#run2{color: green}"),
+             HTML("#run3{color: green}"),
+             HTML("#run4{color: green}"),
+             HTML("#run5{color: green}"),
+             HTML("#run6{color: green}"),
+             HTML("#run7{color: green}")
+            ),
   
   mainPanel(
-    width = 12, align = "center",
-    actionButton("run1","beer-mug-empty", class = "btn-link btn-lg", icon = icon("beer-mug-empty")), br(),
-  actionButton("run2","bandage", class = "btn-link btn-lg",icon = icon("bandage")), br(),
-  actionButton("run3","archway", class = "btn-link btn-lg",icon = icon("archway")), br(),
-  actionButton("run4","apple-whole", class = "btn-link btn-lg",icon = icon("apple-whole")), br(),
-  actionButton("run5","bottle-droplet", class = "btn-link btn-lg",icon = icon("bottle-droplet")), br(),
-  actionButton("run6","sack-dollar", class = "btn-link btn-lg",icon = icon("sack-dollar")), br(),
-  actionButton("run7","cash-register", class = "btn-link btn-lg",icon = icon("cash-register")), br(),
-  actionButton("run8","dungeon", class = "btn-link btn-lg",icon = icon("dungeon")), br(),
-  actionButton("run9","fort-awesome", class = "btn-link btn-lg",icon = icon("fort-awesome")), br(),
-  actionButton("run10","horse", class = "btn-link btn-lg",icon = icon("horse")), br(),
-  actionButton("run11","mortar-pestle", class = "btn-link btn-lg",icon = icon("mortar-pestle")), br(),
-  actionButton("run12","scale-balanced ", class = "btn-link btn-lg",icon = icon("scale-balanced")), br(), br(), br()
-  # icon("beer-mug-empty")
+    width = 12,
+    fluidRow(
+    column(
+        width = 6,
+        align = "right",
+        actionButton("run1","Alchemist", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run1b","", class = "btn-link btn-lg",icon = icon("mortar-pestle"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run2","Bank", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run2b","", class = "btn-link btn-lg",icon = icon("sack-dollar"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run3","Ferrier", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run3b","", class = "btn-link btn-lg",icon = icon("horse"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run4","Healer", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run4b","", class = "btn-link btn-lg",icon = icon("bandage"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run5","Tavern", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run5b","", class = "btn-link btn-lg",icon = icon("beer-mug-empty"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run6","Trader", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run6b","", class = "btn-link btn-lg",icon = icon("cash-register"))
+      )
+    ),
+    br(),
+    fluidRow(
+      column(
+        width = 6,
+        align = "right",
+        actionButton("run7","Trainer", class = "btn-link btn-lg", style="padding:42px;")),
+      column(
+        width = 6,
+        align = "left",
+        actionButton("run7b","", class = "btn-link btn-lg",icon = icon("dumbbell"))
+      )
+    )
   )
 )
