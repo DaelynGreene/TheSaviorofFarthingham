0c442c10e4c0f505b267b2b75bea41f1874354c5
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-04 14:47:17 -0400

    trying to make it like guineapig

--- server.R
+++ server.R
@@ -2,10 +2,170 @@ library(shiny)
 
 server<-function(input,output,session){
   
+  ClickCounter1 <- reactiveValues(NumberClicks1 = 0)
+  observeEvent(input$run1, {ClickCounter1$NumberClicks1 <- 1})
+  
+  ClickCounter2 <- reactiveValues(NumberClicks2 = 0)
+  observeEvent(input$run1b, {ClickCounter2$NumberClicks2 <- 1})
+  
+  ClickCounter3 <- reactiveValues(NumberClicks3 = 0)
+  observeEvent(input$run2, {ClickCounter3$NumberClicks3 <- 1})
+               
+  ClickCounter4 <- reactiveValues(NumberClicks4 = 0)
+  observeEvent(input$run2b, {ClickCounter4$NumberClicks4 <- 1})            
+               
+  ClickCounter5 <- reactiveValues(NumberClicks5 = 0)
+  observeEvent(input$run3, {ClickCounter5$NumberClicks5 <- 1})           
+               
+  ClickCounter6 <- reactiveValues(NumberClicks6 = 0)
+  observeEvent(input$run3b, {ClickCounter6$NumberClicks6 <- 1})           
+               
+  ClickCounter7 <- reactiveValues(NumberClicks7 = 0)
+  observeEvent(input$run4, {ClickCounter7$NumberClicks7 <- 1})
+               
+  ClickCounter8 <- reactiveValues(NumberClicks8 = 0)
+  observeEvent(input$run4b, {ClickCounter8$NumberClicks8 <- 1})
+               
+  ClickCounter9 <- reactiveValues(NumberClicks9 = 0)
+  observeEvent(input$run5, {ClickCounter9$NumberClicks9 <- 1})
+               
+  ClickCounter10 <- reactiveValues(NumberClicks10 = 0)
+  observeEvent(input$run5b, {ClickCounter10$NumberClicks10 <- 1})
+               
+  ClickCounter11 <- reactiveValues(NumberClicks11 = 0)
+  observeEvent(input$run6, {ClickCounter11$NumberClicks11 <- 1})
+               
+  ClickCounter12 <- reactiveValues(NumberClicks12 = 0)
+  observeEvent(input$run6b, {ClickCounter12$NumberClicks12 <- 1})
+  
+  ClickCounter13 <- reactiveValues(NumberClicks13 = 0)
+  observeEvent(input$run7, {ClickCounter13$NumberClicks13 <- 1})
+  
+  ClickCounter14 <- reactiveValues(NumberClicks14 = 0)
+  observeEvent(input$run7b, {ClickCounter14$NumberClicks14 <- 1})
+               
   
   
+  observeEvent(input$reset1, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+               
+  observeEvent(input$reset2, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset3, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset4, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset5, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset6, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset7, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset8, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset9, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset10, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset11, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset12, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset13, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
+  
+  observeEvent(input$reset14, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0;ClickCounter3$NumberClicks3;ClickCounter4$NumberClicks4;ClickCounter5$NumberClicks5;ClickCounter6$NumberClicks6;ClickCounter7$NumberClicks7;ClickCounter8$NumberClicks8;ClickCounter9$NumberClicks9;ClickCounter10$NumberClicks10;ClickCounter11$NumberClicks11;ClickCounter12$NumberClicks12;ClickCounter13$NumberClicks13;ClickCounter14$NumberClicks14})
   
   
-  
+  output$SaviorOfFarthingham <- renderUI({
+               if ((ClickCounter1$NumberClicks1 &  ClickCounter2$NumberClicks2 &  ClickCounter3$NumberClicks3 &  ClickCounter4$NumberClicks4 &  ClickCounter5$NumberClicks5 &  ClickCounter6$NumberClicks6 &  ClickCounter7$NumberClicks7 &  ClickCounter8$NumberClicks8 &  ClickCounter9$NumberClicks9 &  ClickCounter10$NumberClicks10 &  ClickCounter11$NumberClicks11 &  ClickCounter12$NumberClicks12 &  ClickCounter13$NumberClicks13 &  ClickCounter14$NumberClicks14) == 0) {
+                 div(mainPanel(
+                 width = 12,
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run1","Alchemist", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run1b","", class = "btn-link btn-lg",icon = icon("mortar-pestle",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run2","Bank", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run2b","", class = "btn-link btn-lg",icon = icon("sack-dollar",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run3","Ferrier", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run3b","", class = "btn-link btn-lg",icon = icon("horse",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run4","Healer", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run4b","", class = "btn-link btn-lg",icon = icon("bandage",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run5","Tavern", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run5b","", class = "btn-link btn-lg",icon = icon("beer-mug-empty",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run6","Trader", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run6b","", class = "btn-link btn-lg",icon = icon("cash-register",verify_fa = FALSE))
+                   )
+                 ),
+                 br(),
+                 fluidRow(
+                   column(
+                     width = 6,
+                     align = "right",
+                     actionButton("run7","Trainer", class = "btn-link btn-lg", style="padding:42px;")),
+                   column(
+                     width = 6,
+                     align = "left",
+                     actionButton("run7b","", class = "btn-link btn-lg",icon = icon("dumbbell",verify_fa = FALSE))
+                   )
+                 )
+               ))
+               } else if (ClickCounter1$NumberClicks1 == 1) {
+    h3("Button 1 has been clicked.")
+  }
+  })
   
 }
--- ui.R
+++ ui.R
@@ -1,93 +1,17 @@
 ui<-fluidPage(
   
   tags$style("@import url(https://use.fontawesome.com/releases/v6.1.1/css/all.css);",
-             HTML(".fa{font-size: 80px; color:#FF8200; align-middle}"),
-             HTML(".fa-fort-awesome{font-size: 100px; color:#FF8200}"),
-             HTML("#run1{color: green}"),
-             HTML("#run2{color: green}"),
-             HTML("#run3{color: green}"),
-             HTML("#run4{color: green}"),
-             HTML("#run5{color: green}"),
-             HTML("#run6{color: green}")
+             HTML(".fa{font-size: 80px; color:#00d6d6; align-middle}"),
+             HTML(".fa-fort-awesome{font-size: 100px; color:#00d6d6}"),
+             HTML("#run1{color: #00d6d6}"),
+             HTML("#run2{color: #00d6d6}"),
+             HTML("#run3{color: #00d6d6}"),
+             HTML("#run4{color: #00d6d6}"),
+             HTML("#run5{color: #00d6d6}"),
+             HTML("#run6{color: #00d6d6}"),
+             HTML("#run7{color: #00d6d6}")
             ),
   
+  uiOutput("SaviorOfFarthingham")
   
-  ##########################################################################################################################################################################
-  #try throwing all of this to the server and in a renderUi that is contingent on a back button being pressed. Generate each submenu based on the clicking of the icon/word beside it
-  #when new page is rendered, reset value of clicked actionButton to 0, that may fix the deal with the page updating properly, use adam's if else statement to make it work properly.
-  
-  mainPanel(
-    width = 12,
-    fluidRow(
-    column(
-        width = 6,
-        align = "right",
-        actionButton("run1","Alchemist", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run1b","", class = "btn-link btn-lg",icon = icon("mortar-pestle",verify_fa = FALSE))
-      )
-    ),
-    br(),
-    fluidRow(
-      column(
-        width = 6,
-        align = "right",
-        actionButton("run2","Bank", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run2b","", class = "btn-link btn-lg",icon = icon("sack-dollar",verify_fa = FALSE))
-      )
-    ),
-    br(),
-    fluidRow(
-      column(
-        width = 6,
-        align = "right",
-        actionButton("run3","Ferrier", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run3b","", class = "btn-link btn-lg",icon = icon("horse",verify_fa = FALSE))
-      )
-    ),
-    br(),
-    fluidRow(
-      column(
-        width = 6,
-        align = "right",
-        actionButton("run4","Healer", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run4b","", class = "btn-link btn-lg",icon = icon("bandage",verify_fa = FALSE))
-      )
-    ),
-    br(),
-    fluidRow(
-      column(
-        width = 6,
-        align = "right",
-        actionButton("run5","Tavern", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run5b","", class = "btn-link btn-lg",icon = icon("beer-mug-empty",verify_fa = FALSE))
-      )
-    ),
-    br(),
-    fluidRow(
-      column(
-        width = 6,
-        align = "right",
-        actionButton("run6","Trader", class = "btn-link btn-lg", style="padding:42px;")),
-      column(
-        width = 6,
-        align = "left",
-        actionButton("run6b","", class = "btn-link btn-lg",icon = icon("cash-register",verify_fa = FALSE))
-      )
-    )
   )
-)
