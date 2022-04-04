38ae96b525ef8c62b5fca8c85ef96435efc05136
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-04 12:29:29 -0400

    made guineapig and got it working to scrub for main

---
+++ guineapig.R
@@ -0,0 +1,37 @@
+library(shiny)
+
+ui <- fluidPage(
+  uiOutput("screen")
+)
+
+server <- function(input, output, session) {
+  
+  ClickCounter1 <- reactiveValues(NumberClicks1 = 0)
+  observeEvent(input$a, {ClickCounter1$NumberClicks1 <- ClickCounter1$NumberClicks1 + 1})  
+  
+  ClickCounter2 <- reactiveValues(NumberClicks2 = 0)
+  observeEvent(input$b, {ClickCounter2$NumberClicks2 <- ClickCounter2$NumberClicks2 + 1})
+  
+  observeEvent(input$reset1, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0})
+  
+  observeEvent(input$reset2, {ClickCounter1$NumberClicks1 <- 0;ClickCounter2$NumberClicks2 <- 0})
+  
+  output$screen <- renderUI({
+    if (ClickCounter1$NumberClicks1 == 0 & ClickCounter2$NumberClicks2 == 0) {
+      div(actionButton('a', 'A'),
+      actionButton('b', 'B'))
+    } else if (ClickCounter1$NumberClicks1 == 1) {
+      div(h3("Button A was clicked."),
+      actionButton("reset1","Reset"))
+    } else {
+      div(h3("Button B was clicked."),
+      actionButton("reset2","Reset"))
+    }
+  })
+}
+
+shinyApp(ui, server)
+
+
+
+# arrow-left
