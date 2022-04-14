f9b4711f2e9a15c901ef7a57c279ac2c7b6174c8
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-14 14:39:19 -0400

    Create sliderworking.R

---
+++ sliderworking.R
@@ -0,0 +1,20 @@
+ui <- fluidPage(
+  sliderInput("obs", "Number of observations:",
+              min = 0, max = 1000, value = 500
+  ),
+  textOutput("distPlot")
+)
+
+# Server logic
+server <- function(input, output) {
+  
+  observeEvent(input$obs,{if (input$obs <=800){output$distPlot <- renderText({
+    (input$obs)
+  })} else {output$distPlot <- renderText({print("TOO MUCH")})}})
+  
+  
+}
+
+
+# Complete app with UI and server components
+shinyApp(ui, server)
