fd49951e21d0a2eacf6d966c5a48d2560e0b48d1
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-14 14:54:59 -0400

    Update sliderworking.R

--- sliderworking.R
+++ sliderworking.R
@@ -2,19 +2,23 @@ ui <- fluidPage(
   sliderInput("obs", "Number of observations:",
               min = 0, max = 1000, value = 500
   ),
-  textOutput("distPlot")
+  textOutput("distPlot"),
+  
+  progressBar(id = "pb8", value = 1500, total = 5000, status = "info", display_pct = TRUE, striped = TRUE, title = "All options")
 )
 
 # Server logic
-server <- function(input, output) {
+server <- function(input, output,session) {
   
   observeEvent(input$obs,{if (input$obs <=800){output$distPlot <- renderText({
     (input$obs)
-  })} else {output$distPlot <- renderText({print("TOO MUCH")})}})
+  })} else {output$distPlot <- renderText({"TOO MUCH"})}})
   
-  
+  updateProgressBar(session = session, id = "pb8", value = input$slider, total = 5000)
 }
 
 
 # Complete app with UI and server components
 shinyApp(ui, server)
+
+
