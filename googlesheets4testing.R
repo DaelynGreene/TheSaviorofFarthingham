8368ec2911981de7c1f2fb5c607915ede8fa3bf0
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-20 14:09:34 -0400

    Create googlesheets4testing.R

---
+++ googlesheets4testing.R
@@ -0,0 +1,5 @@
+library(googlesheets4)
+
+mydata <- data.frame(c(1,2,3,4,5,6,6))
+
+read_sheet("https://docs.google.com/spreadsheets/d/1Y-YpcWe5L0sTvGNe1h3ButMRXV2dWDtzv2h0quKdj6s/edit?usp=sharing")
