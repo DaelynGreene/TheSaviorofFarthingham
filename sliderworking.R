95bd99e060681fc34de503d0bd51e6e2c124daea
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-25 16:55:50 -0400

    added adam's randomizer

--- sliderworking.R
+++ sliderworking.R
@@ -6,7 +6,7 @@ ui <- fluidPage(
   uiOutput("sliders1"),
   uiOutput("sliders2"),
   uiOutput("sliders3"),
-  uiOutput("words")
+  uiOutput("button")
 )
 server <- function(input, output) {
   observeEvent(input$obs1 | input$obs2 | input$obs3,{
@@ -17,7 +17,7 @@ server <- function(input, output) {
           align = "center",
           sliderInput(
       "obs1",
-      "Skill 1:",
+      paste("Skill 1:",input$obs1),
       min = 0,
       max = min(10,20-sum(input$obs2,input$obs3)),
       value = if (is.null(input$obs1)) {0} else{input$obs1},
@@ -30,7 +30,7 @@ server <- function(input, output) {
           align = "center",
       sliderInput(
       "obs2",
-      "Skill 2:",
+      paste("Skill 2:",input$obs2),
       min = 0,
       max = min(10,20-sum(input$obs1,input$obs3)),
       value = if (is.null(input$obs2)) {0} else{input$obs2},
@@ -43,25 +43,76 @@ server <- function(input, output) {
           align = "center",
       sliderInput(
       "obs3",
-      "Skill 3:",
+      paste("Skill 3:",input$obs3),
       min = 0,
       max = min(10,20-sum(input$obs1,input$obs2)),
       value = if (is.null(input$obs3)) {0} else{input$obs3},
       step = 1)
   )})})
-    output$words <- renderUI({  
+    output$button <- renderUI({  
     fluidRow({
         column(
           width = 12,
           align = "center",
           h4(paste(20-sum(input$obs1,input$obs2,input$obs3),"skill points remaining.")),
-          actionButton("Randomize","Randomize")
+          actionButton("AdamsRandomize","Randomize")
         )
       })
     })
   })
-  # observeEvent(input$Randomize,{
-  #   
-  # })
+  observeEvent(input$AdamsRandomize,{
+    Numbers <<- sample(1:10,3,replace = T)
+    if (sum(Numbers) != 20) {
+      Remove <<- sample(1:3,1)
+      TwoNumberSum <<- sum(Numbers[-Remove])
+      while(TwoNumberSum<10){
+        Numbers <<- sample(1:10,3,replace = T)
+        Remove <<- sample(1:3,1)
+        TwoNumberSum <<- sum(Numbers[-Remove])
+      }
+      Numbers[Remove] <<- 20-TwoNumberSum
+    }
+    
+    output$sliders1 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+          sliderInput(
+            "obs1",
+            paste("Skill 1:",input$obs1),
+            min = 0,
+            max = Numbers[1],
+            value = Numbers[1],
+            step = 1)
+        )})})
+    output$sliders2 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+          sliderInput(
+            "obs2",
+            paste("Skill 2:",input$obs2),
+            min = 0,
+            max = Numbers[2],
+            value = Numbers[2],
+            step = 1)
+        )})})
+    output$sliders3 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+          sliderInput(
+            "obs3",
+            paste("Skill 3:",input$obs3),
+            min = 0,
+            max = Numbers[3],
+            value = Numbers[3],
+            step = 1)
+        )})})
+   
+  })
 }
 shinyApp(ui, server)
