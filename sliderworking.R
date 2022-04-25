0edb9a6a83a42e1978cbbfb3999a4b4098de7edd
Daelyn Greene <dgreen41@vols.utk.edu>
2022-04-25 15:36:17 -0400

    Fancified it and made it better

--- sliderworking.R
+++ sliderworking.R
@@ -1,52 +1,67 @@
 library(shiny)
 library(shinyWidgets)
-
 ui <- fluidPage(
-  
+  br(),
+  br(),
   uiOutput("sliders1"),
   uiOutput("sliders2"),
   uiOutput("sliders3"),
-  
   uiOutput("words")
 )
-
-# Server logic
 server <- function(input, output) {
-  
-  observeEvent(input$obs1 | input$obs2 | input$obs3,{if (sum(input$obs1,input$obs2,input$obs3) <=20){output$words <- renderText({
-    (sum(input$obs1,input$obs2,input$obs3))
-  })} else {output$words <- renderUI({h2("TOO MUCH",style = "color: red; font-family: papyrus; font-weight: bolder; font-size: 30px")})}})
-  
-  
   observeEvent(input$obs1 | input$obs2 | input$obs3,{
-    output$sliders1 <- renderUI({sliderInput(
+    output$sliders1 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+          sliderInput(
       "obs1",
-      "Number of observations 1:",
+      "Skill 1:",
       min = 0,
       max = min(10,20-sum(input$obs2,input$obs3)),
       value = if (is.null(input$obs1)) {0} else{input$obs1},
       step = 1)
-    })
-    output$sliders2 <- renderUI({sliderInput(
+    )})})
+    output$sliders2 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+      sliderInput(
       "obs2",
-      "Number of observations 2:",
+      "Skill 2:",
       min = 0,
       max = min(10,20-sum(input$obs1,input$obs3)),
       value = if (is.null(input$obs2)) {0} else{input$obs2},
       step = 1)
-    })
-    output$sliders3 <- renderUI({sliderInput(
+      )})})
+    output$sliders3 <- renderUI({
+      fluidRow({
+        column(
+          width = 12,
+          align = "center",
+      sliderInput(
       "obs3",
-      "Number of observations 3:",
+      "Skill 3:",
       min = 0,
       max = min(10,20-sum(input$obs1,input$obs2)),
       value = if (is.null(input$obs3)) {0} else{input$obs3},
       step = 1)
+  )})})
+    output$words <- renderUI({  
+    fluidRow({
+        column(
+          width = 12,
+          align = "center",
+          h4(paste(20-sum(input$obs1,input$obs2,input$obs3),"skill points remaining.")),
+          actionButton("Randomize","Randomize")
+        )
+      })
     })
   })
-  
+  # observeEvent(input$Randomize,{
+  #   
+  # })
 }
-
-
-# Complete app with UI and server components
 shinyApp(ui, server)
