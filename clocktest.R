a7099ece2ca900196db24a4b87f23c0b1e1c264b
Daelyn Greene <dgreen41@vols.utk.edu>
2022-05-17 16:34:42 -0400

    FIXED THE AM PM TOGGLE FINALLY

--- clocktest.R
+++ clocktest.R
@@ -3,45 +3,46 @@ library(shinyWidgets)
 library(lubridate)
 
 ui <- fluidPage(
-  textOutput("words"),
   textOutput("currentNumber")
 )
 
 server <- function(input, output, session) {
   
-MinutesGoneBy<- reactiveVal(0)
-HourOfDay12 <- reactiveVal(5)
+MinutesGoneBy<- reactiveVal(50)
+HourOfDay <- reactiveVal(5)
 DaysElapsed <- reactiveVal(1)
 NumberOfHours24 <- reactiveVal(17)
+Switch <- reactiveVal(1)
 AmPmToggle <- reactiveVal("pm")
 
+
 observe({
-    invalidateLater(25,session)
-    isolate({
-      MinutesGoneBy(MinutesGoneBy()+1)
-      if(as.numeric(MinutesGoneBy())==60){
-        HourOfDay12(HourOfDay12()+1)
-        MinutesGoneBy(0)
-        NumberOfHours24(NumberOfHours24()+1)
-      }
-      if(as.numeric(NumberOfHours24()==24)){
-        DaysElapsed(DaysElapsed()+1)
-        HourOfDay12(1)
-        AmPmToggle("am")
-      }
-      if(as.numeric(HourOfDay12()) == 12){
-        AmPmToggle("pm")
-      }
-    })
+  invalidateLater(25,session)
+  isolate({
+    MinutesGoneBy(MinutesGoneBy()+1)
+    if(as.numeric(MinutesGoneBy())==60){
+      MinutesGoneBy(0)
+      NumberOfHours24(NumberOfHours24()+1)
+      HourOfDay(HourOfDay()+1)
+    }
+    if(as.numeric(HourOfDay()) == 13){
+      MinutesGoneBy(0)
+      HourOfDay(1)
+    }
+    if (as.numeric(HourOfDay()) == 12 & as.numeric(MinutesGoneBy()) == 0) {
+      Switch(Switch()+1)
+    }
+    if(as.numeric(Switch()) %% 2 == 0){
+      AmPmToggle("am")
+    } else {
+      AmPmToggle("pm")
+    }
+  })
 })
 
 output$currentNumber <- renderText({
-  paste(HourOfDay12(),MinutesGoneBy(),AmPmToggle(),NumberOfHours24())
+  paste(HourOfDay(),MinutesGoneBy(),AmPmToggle(),NumberOfHours24(),Switch())
   })
-
-output$words <- renderText({
-  paste("hour","minutes","hours")
-})
 
 }
 shinyApp(ui, server)
